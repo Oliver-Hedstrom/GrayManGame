032bde8268a091dd0639780534e853a6fb426c03
Kjell Hedstrom <Kjell.Hedstrom@Gmail.com>
2023-12-28 16:12:44 -0700

    basic functionality, moving a dummy with collision detect

--- src/animation.py
+++ src/animation.py
@@ -1,1 +1,47 @@
 #Deals with the different animations - electrifying fence, electrocuting attacker, loss animation.
+# src/animation.py
+# src/animation.py
+import pygame
+
+
+# This is our stick figure - split into lines
+stick_figure = [
+    " O ",
+    "-|-",
+    "/ \\",
+]
+
+# Alternative stick figure for electric impact
+electric_impact_figure = [
+    " \\O/ ",
+    " -|- ",
+    " / \\ ",
+]
+
+
+def check_collision(attacker_pos, fence_x_position, fence_width):
+    # Returns True if the attacker has collided with the fence
+    return attacker_pos >= fence_x_position and attacker_pos < (fence_x_position + fence_width)
+
+def animate_attacker(screen, font, attacker_pos, fence_x_position, fence_width, distance):
+    # Define colors
+    RED = (255, 0, 0)
+    WHITE = (255, 255, 255)
+    
+    # Check for collision
+    collision = check_collision(attacker_pos, fence_x_position, fence_width)
+    current_figure = electric_impact_figure if collision else stick_figure
+
+    # Determine the color based on collision
+    color = RED if collision and (pygame.time.get_ticks() // 250) % 2 else WHITE
+    
+    # Optionally update ASCII art for electric impact
+    if collision:
+        # stick_figure = updated ASCII art for electric impact
+        pass
+    
+    # Blit each line of ASCII art as a separate text Surface
+    for i, line in enumerate(current_figure):
+        s = font.render(line, True, color)
+        pos = (distance, i * 50)  # 50 is roughly the height of each line
+        screen.blit(s, pos)
--- src/main.py
+++ src/main.py
@@ -1,29 +1,43 @@
 import pygame
+from pygame.locals import *
 import sys
+from animation import animate_attacker 
 
 pygame.init()
 
 # Constants
-WIDTH, HEIGHT = 800, 600
 FPS = 60
 
 # Colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+GREY = (128, 128, 128)
 
-# This is our stick figure - split into lines
-stick_figure = [
-    " O ",
-    "-|-",
-    "/ \\",
-]
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+screen_info = pygame.display.Info()
+# Calculate 90% of the screen width
+screen_width = int(screen_info.current_w/2)
+screen_height = int(screen_info.current_h/4)
+
+screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 50)  # You can choose your own font and size
 
 running = True
 distance = 0  # Distance from left screen edge
+
+# Load the fence image
+fence_image = pygame.image.load('resources/sprites/fence.png')
+# Scale the image to be 5 times bigger
+fence_image = pygame.transform.scale(fence_image, (fence_image.get_width() * 3, fence_image.get_height() * 3))
+# Get the rectangle of the scaled image
+fence_rect = fence_image.get_rect()
+fence_x_position = int(screen_width * 0.75)
+fence_width = fence_rect.width
+# Calculate how many times the image should be repeated to fill the screen height
+repeat_count = screen_height // fence_rect.height + 1
+
 
 while running:
     clock.tick(FPS)
@@ -33,18 +47,26 @@ while running:
             running = False
     
     # Fill the screen with black
-    screen.fill(BLACK)
+    screen.fill(GREY)
+
+    # Blit the scaled fence image repeatedly to create a contiguous line
+    for i in range(repeat_count):
+        screen.blit(fence_image, (fence_x_position, i * fence_rect.height))
+
+# ... rest of your game loop code ...
+
     
     # Blit each line of ASCII art as a separate text Surface
-    for i, line in enumerate(stick_figure):
-        s = font.render(line, True, WHITE)
-        pos = (distance, i * 50)  # 50 is roughly the height of each line
-        screen.blit(s, pos)
+    # for i, line in enumerate(stick_figure):
+    #     s = font.render(line, True, WHITE)
+    #     pos = (distance, i * 50)  # 50 is roughly the height of each line
+    #     screen.blit(s, pos)
+    animate_attacker(screen, font, distance, fence_x_position, fence_width, distance)
     
     distance += 1  # Move 1px/frame to the right
 
     # If figure moved off screen, reset back to left edge
-    if distance > WIDTH:
+    if distance > screen_width:
         distance = 0
     
     # Flip the display
